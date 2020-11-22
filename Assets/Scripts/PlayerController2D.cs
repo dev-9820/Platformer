3d9a1bde813879262a0c6bfffa53d3180e0c3b0a
karolis <nikarka1@gmail.com>
2020-11-22 22:43:08 +0100

    Added some 2d components and player movement

---
+++ Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;                //Floating point variable to store the player's movement speed.
+
+    [SerializeField]
+    private float jumpScale;
+    private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D> ();
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpScale);
+        }
+    }
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        rb2d.velocity = new Vector2(moveHorizontal*speed, rb2d.velocity.y);
+    }
+}
